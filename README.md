c7c7dfc0acd50c34131df38d42dc746499ccb994
Leonid_Shvetsov <ls.shvetsov@gmail.com>
2024-05-23 08:01:28 +0400

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+### Simple text editor in Java (Swing)
+
+**Functions**
+1. Load file (txt)
+2. Edit file
+3. Save file
+4. Search by string and regexp
---
+++ src/main/java/ge/shvetsov/ApplicationRunner.java
@@ -0,0 +1,7 @@
+package ge.shvetsov;
+
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        new TextEditor();
+    }
+}
